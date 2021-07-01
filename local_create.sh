d5f60002e619a9f47078df0fdecd590b14134a67
zhengqihang <597323109@qq.com>
2021-07-01 14:29:57 +0800

    add flatbuffers

--- local_create.sh
+++ local_create.sh
@@ -5,3 +5,7 @@ cd ClangPkg/all
 conan create . 12.0.0@ -s build_type=Debug && cd ../..
 cd HalidePkg/all
 conan create . 12.0.0@ -s build_type=Debug && cd ../..
+
+cd flatbuffersPkg/all
+conan create . 2.0.0@ -o flatbuffers:options_from_context=False
+conan upload flatbuffers/2.0.0 --all -r=sunnycase
