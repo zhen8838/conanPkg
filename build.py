5d3c801636604b0d88544bb8f2898dfca9bb883b
郑启航 <597323109@qq.com>
2021-07-02 14:28:53 +0800

    add auto package

--- build.py
+++ build.py
@@ -7,14 +7,11 @@ import yaml
 if __name__ == "__main__":
   with open('configs.yaml', 'r') as f:
     configs = yaml.safe_load(f)
-  for config in configs:
-    print(config)
-    build_configs = config.pop('build_configs')
-    if build_configs is None:
-      build_configs = {'settings': None, 'options': None, 'env_vars': None, 'build_requires': None}
-    builder = ConanMultiPackager(**config)
-    for build_config in build_configs:
-      for build_type in ['Debug', 'Release']:
-        build_config['settings'] = {'build_type': build_type}
-        builder.add(**build_config)
+  for config in configs['packages']:
+    package_configs = config.pop('package_configs')
+    common_build_configs = config.pop('common_build_configs')
+    if common_build_configs is None:
+      build_configs = {}
+    builder = ConanMultiPackager(**package_configs)
+    builder.add_common_builds(**common_build_configs)
     builder.run()
