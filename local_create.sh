02a379ec8c3349910d157f635075538c49006433
zhengqihang <597323109@qq.com>
2021-12-02 12:00:43 +0000

    fix create shell

--- local_create.sh
+++ local_create.sh
@@ -1,10 +1,16 @@
 #!/bin/zsh
+# cd LLVM_corePkg/all
+# conan create . 12.0.0@ -s build_type=Release && cd ../..
+# cd ClangPkg/all
+# conan create . 12.0.0@ -s build_type=Release && cd ../..
+# cd HalidePkg/all
+# conan create . 12.0.0@ -s build_type=Release && cd ../..
 cd LLVM_corePkg/all
-conan create . 12.0.0@ -s build_type=Release && cd ../..
+conan export . 12.0.0@ && cd ../..
 cd ClangPkg/all
-conan create . 12.0.0@ -s build_type=Release && cd ../..
+conan export . 12.0.0@ && cd ../..
 cd HalidePkg/all
-conan create . 12.0.0@ -s build_type=Release && cd ../..
+conan export . 12.0.0@ && cd ../..
 
 # cd flatbuffersPkg/all
 # conan create . 2.0.0@ -o flatbuffers:options_from_context=False
