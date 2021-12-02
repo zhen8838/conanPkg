8cca014ce746834cd2626446557a6e9d03099c1d
zhengqihang <597323109@qq.com>
2021-12-02 11:24:41 +0800

    add local package create

--- local_create.sh
+++ local_create.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 cd LLVM_corePkg/all
-conan create . 12.0.0@ -s build_type=Debug && cd ../..
+conan create . 12.0.0@ -s build_type=Release && cd ../..
 cd ClangPkg/all
-conan create . 12.0.0@ -s build_type=Debug && cd ../..
+conan create . 12.0.0@ -s build_type=Release && cd ../..
 cd HalidePkg/all
-conan create . 12.0.0@ -s build_type=Debug && cd ../..
+conan create . 12.0.0@ -s build_type=Release && cd ../..
 
-cd flatbuffersPkg/all
-conan create . 2.0.0@ -o flatbuffers:options_from_context=False
-conan upload flatbuffers/2.0.0 --all -r=sunnycase
+# cd flatbuffersPkg/all
+# conan create . 2.0.0@ -o flatbuffers:options_from_context=False
+# conan upload flatbuffers/2.0.0 --all -r=sunnycase
